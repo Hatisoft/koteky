8709d4ce94bb7912e9df6c5e8dcd13cc3a10ffa0
kanekotic <alvarojosepl@gmail.com>
2016-02-21 20:37:34 +0100

    added working plugin loader and updater

---
+++ index.js
@@ -0,0 +1,42 @@
+var updater = require('electron-updater');
+var	plugins = require('electron-plugins');
+var	util = require('util');
+var	ipc = require('ipc');
+
+document.addEventListener('DOMContentLoaded', function () {
+	var context = { document: document };
+	plugins.load(context, function (err, loaded) {
+		if(err)
+			return console.log(util.inspect(err));
+		console.log('Plugins loaded successfully.');
+	});
+});
+
+ipc.on('update-available', function () {
+	console.log('there is an update available for download');
+});
+
+function check() {
+	updater.check(function (err, results) {
+		if(err)
+			return console.error(err);
+		var output = document.getElementById('output');
+		var li = document.createElement('li');
+		if(results)
+			li.innerHTML = 'update available: ' + (util.inspect(results).replace(/\n/g, '<br>'));
+		else
+			li.innerHTML = 'no update available.';
+		output.appendChild(li);
+	});
+}
+
+function list() {
+	updater.list(function (err, dependencies) {
+		if(err)
+			return console.error(err);
+		var output = document.getElementById('output');
+		var li = document.createElement('li');
+		li.innerHTML = 'dependencies: ' + (util.inspect(dependencies).replace(/\n/g, '<br>'));
+		output.appendChild(li);
+	});
+}
--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 var app = require('app');
+var ipc = require('ipc');
+var util = require('util');
 var BrowserWindow = require('browser-window');
 var updater = require('electron-updater');
 
-app.on('ready', function () {
+require('crash-reporter').start();
+
+var mainWindow = null;
+var loaded = false;
+
+app.on('window-all-closed', function() {
+    if (process.platform != 'darwin')
+        app.quit();
+});
+
+app.on('ready', function() {
     updater.on('ready', function () {
         mainWindow = new BrowserWindow({width: 800, height: 600});
-        mainWindow.loadUrl('file://' + __dirname + '/src/index.html');
+        mainWindow.loadUrl('file://' + __dirname + '/index.html');
         mainWindow.openDevTools({detach:true});
         mainWindow.on('closed', function() {
             mainWindow = null;
@@ -15,18 +27,8 @@ app.on('ready', function () {
         app.quit();
     });
     updater.on('updateAvailable', function () {
-        mainWindow.webContents.send('update-available');
+        if(mainWindow)
+            mainWindow.webContents.send('update-available');
     });
-    updater.on('error', function (err) {
-        console.log(err);
-    });
-
-    var customLogger = {
-      log: console.log,
-      error: console.error,
-      info: console.info,
-      warn: console.warn,
-      debug: console.debug
-    };
-    updater.start(customLogger);
+    updater.start();
 });
