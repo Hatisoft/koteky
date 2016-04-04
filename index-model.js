cfa51caf1480368e90af825eec6662f95c5c32b2
kanekotic <alvarojosepl@gmail.com>
2016-04-04 09:05:25 +0200

    fixed integration with vue.js

--- index-model.js
+++ index-model.js
@@ -1,7 +1,8 @@
-
+"use strict"
 module.exports = class IndexModel{
 	constructor()
 	{
 	 this.posts = new Array();
+	 this.example = [ {name: "Test1"},{name: "Test2"},{name: "Test3"}];
 	}
 };
--- index-view-model.js
+++ index-view-model.js
@@ -7,7 +7,7 @@ var	ipc = require('ipc');
 
 module.exports = class IndexViewModel{
     constructor (view, model){
-        /*view.document.addEventListener('DOMContentLoaded', function () {
+        view.document.addEventListener('DOMContentLoaded', function () {
         	var context = { document: document, posts : this.posts };
         	plugins.load(context, function (err, loaded) {
         		if(err)
@@ -15,19 +15,12 @@ module.exports = class IndexViewModel{
         		console.log('Plugins loaded successfully.');
         	});
         });
-
         ipc.on('update-available', function () {
-        	console.log('there is an update available for download');
-        });*/
-
-        this.vue = new vue({
-                el: view,
+            console.log('there is an update available for download');
+        });
+        new vue({
+                el: "#app",
                 data: model
-                /*methods:
-                    close: () ->
-                        @$data.onClose()
-                    select: (index) ->
-                        @$data.onSelectIndex(index)*/
-                    });
+        });
     }
 };
