bb504aa5b10e6470adeafdbbef35cc8b7988670f
kanekotic <alvarojosepl@gmail.com>
2016-05-07 08:43:24 +0200

    added menu bar

--- main.js
+++ main.js
@@ -1,15 +1,35 @@
 var electron = require('electron');
 var BrowserWindow = electron.BrowserWindow;
 var ipcMain = electron.ipcMain;
+var contextMenuBar = electron.Menu;
 var updater = require('electron-updater');
 var menubar = require('menubar');
 
+
 var options = {dir: __dirname, index: 'file://' + __dirname + '/index.html', 'preload-window': true};
+
 var menu = menubar(options);
+
+var menuTemplate=[
+      {
+        label: 'Options',
+        click: () => {
+            //to be implemented
+        }
+      },
+      {
+        label: 'Close',
+        click: () => {
+              menu.app.quit();
+        }
+    }];
 
 menu.on('ready', function() {
     updater.on('ready', function() {
 
+        const contextMenu = contextMenuBar.buildFromTemplate(menuTemplate);
+          //menu.tray.setToolTip('This is my application.');
+          menu.tray.setContextMenu(contextMenu);
     });
     updater.on('updateRequired', function () {
         menu.app.quit();
