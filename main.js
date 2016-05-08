6c61cbf73010e4adf2df493a18c4f6f566b40ecf
kanekotic <alvarojosepl@gmail.com>
2016-05-08 18:41:55 +0200

    added persistency mechanism

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
+"use strict";
 var electron = require('electron');
 var BrowserWindow = electron.BrowserWindow;
 var ipcMain = electron.ipcMain;
 var contextMenuBar = electron.Menu;
 var updater = require('electron-updater');
 var menubar = require('menubar');
-
+var ElectronSettings = require('electron-settings');
 
 var options = {dir: __dirname, index: 'file://' + __dirname + '/index.html', 'preload-window': true};
-
-var menu = menubar(options);
+var settings = new ElectronSettings();
+var windowsSettings = settings.get('window');
+if(windowsSettings)
+    Object.keys(windowsSettings).forEach(function(key) { options[key] = windowsSettings[key]; });
 
 var menuTemplate=[
       {
         label: 'Options',
         click: () => {
+            //var localwindow = new BrowserWindow({show: false, webPreferences: {nodeIntegration: false}});
             //to be implemented
         }
       },
@@ -23,6 +27,8 @@ var menuTemplate=[
               menu.app.quit();
         }
     }];
+
+var menu = menubar(options);
 
 menu.on('ready', function() {
     updater.on('ready', function() {
@@ -42,5 +48,25 @@ menu.on('ready', function() {
 });
 
 menu.on('after-create-window', function() {
+    menu.window.on('resize', function() {
+        settings.set('window.height',  menu.window.getSize()[0]);
+        settings.set('window.width',  menu.window.getSize()[1]);
+    });
+    menu.window.on('move', function() {
+        settings.set('window.x',  menu.window.getPosition()[0]);
+        settings.set('window.y',  menu.window.getPosition()[1]);
+    });
+    menu.window.on('close', function () {
+        if (!menu.window)
+            return;
+        var bounds = menu.window.getBounds();
+        settings.set("window", {
+              x: bounds.x,
+              y: bounds.y,
+              width: bounds.width,
+              height: bounds.height
+            });
+    });
     menu.window.openDevTools({detach:true});
+
 });
